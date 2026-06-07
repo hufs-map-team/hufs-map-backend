1ec9e449b05dd10d4012ef984560a76b56c1fc08
Sunhu Park <sunhu0104@gmail.com>
2026-06-07 19:42:34 +0900

    건물 즐겨찾기 업데이트 로직 추가

--- src/lib/userService.js
+++ src/lib/userService.js
@@ -23,3 +23,20 @@ export const getCurrentUserRow = async () => {
 
   return rows[0];
 };
+export const updateFavoriteBuildings = async (buildingKey) => {
+  const userRow = await getCurrentUserRow();
+  const currentFavorites = userRow.favoriteBuildings || [];
+
+  const nextFavorites = currentFavorites.includes(buildingKey)
+    ? currentFavorites.filter((building) => building !== buildingKey)
+    : [...currentFavorites, buildingKey];
+
+  return tablesDB.updateRow({
+    databaseId: DATABASE_ID,
+    tableId: USERS_TABLE_ID,
+    rowId: userRow.$id,
+    data: {
+      favoriteBuildings: nextFavorites,
+    },
+  });
+};
